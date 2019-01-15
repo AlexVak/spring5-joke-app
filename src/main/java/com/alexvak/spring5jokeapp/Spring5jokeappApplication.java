2afb9097d20386a018063ace616cdda797e8dd93
AlexVak <avakarchuk@me.com>
2019-01-15 17:15:39 +0200

    xml config example

--- src/main/java/com/alexvak/spring5jokeapp/Spring5jokeappApplication.java
+++ src/main/java/com/alexvak/spring5jokeapp/Spring5jokeappApplication.java
@@ -2,8 +2,10 @@ package com.alexvak.spring5jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:app-config.xml")
 public class Spring5jokeappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/alexvak/spring5jokeapp/config/AppConfig.java
+++ src/main/java/com/alexvak/spring5jokeapp/config/AppConfig.java
@@ -1,13 +1,11 @@
 package com.alexvak.spring5jokeapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class AppConfig {
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
